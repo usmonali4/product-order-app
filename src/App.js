ec68f63a56c672b72b0c341b61b7d26920e11b5f
Usmon <usmonpahlavonov@gmail.com>
2024-05-07 16:26:51 +0900

    updated Handling Http States using and Action Creator Thunk

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {Fragment, useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification'
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -16,54 +16,12 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-      const response = await fetch('https://product-order-app-default-rtdb.firebaseio.com/cart.json', {
-          method: 'PUT', 
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if(!response.ok){
-        dispatch(
-          uiActions.showNotification({
-            status: 'error',
-            title: 'Error!',
-            message: 'Sending cart data failed!',
-          })
-        );
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    };
-
     if(isInitial){
       isInitial = false;
       return;
     }
 
-    sendCartData().catch(error => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
-
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
